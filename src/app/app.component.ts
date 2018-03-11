23d9f01a39c557faa0cb897f97917a33d616773c
Jordi Díaz Añorga <jdiaz@plainconcepts.com>
2018-03-11 21:00:52 +0100

    Not perform heavy computations in templates.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,13 @@ import { Names } from './names';
 
 const NumRange: [number, number] = [23, 28];
 
+const fibonacci = (num: number): number => {
+  if (num === 1 || num === 2) {
+    return 1;
+  }
+  return fibonacci(num - 1) + fibonacci(num - 2);
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -19,14 +26,14 @@ export class AppComponent implements OnInit {
   public onAddItemToList1(label: string): void {
     this.list1Items.unshift({
       label: label,
-      num: this.generator.generateNumber(NumRange)
+      num: this.calculate(this.generator.generateNumber(NumRange))
     });
   }
 
   public onAddItemToList2(label: string): void {
     this.list2Items.unshift({
       label: label,
-      num: this.generator.generateNumber(NumRange)
+      num: this.calculate(this.generator.generateNumber(NumRange))
     });
   }
 
@@ -38,8 +45,17 @@ export class AppComponent implements OnInit {
     this.list1Items.splice(this.list1Items.indexOf(item), 1);
   }
 
+  private calculate(num: number): number {
+    console.log('Calculating...');
+    return fibonacci(num);
+  }
+
   public ngOnInit(): void {
-    this.list1Items = this.generator.generate(Names, NumRange, 140);
-    this.list2Items = this.generator.generate(Names, NumRange, 140);
+    this.list1Items = this.generator.generate(Names, NumRange, 140).map(i => {
+      return { label: i.label, num: this.calculate(i.num) };
+    });
+    this.list2Items = this.generator.generate(Names, NumRange, 140).map(i => {
+      return { label: i.label, num: this.calculate(i.num) };
+    });
   }
 }
--- src/app/random-list/random-list.component.ts
+++ src/app/random-list/random-list.component.ts
@@ -1,29 +1,18 @@
 import { ListItem } from '../list-generator';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-const fibonacci = (num: number): number => {
-  if (num === 1 || num === 2) {
-    return 1;
-  }
-  return fibonacci(num - 1) + fibonacci(num - 2);
-};
-
 @Component({
   selector: 'app-random-list',
   templateUrl: './random-list.component.html',
   styleUrls: ['./random-list.component.css']
 })
 export class RandomListComponent {
-
   public label: string;
 
-  @Input()
-  public listItems: ListItem[];
+  @Input() public listItems: ListItem[];
 
-  @Output()
-  private addItem: EventEmitter<string> = new EventEmitter<string>();
-  @Output()
-  private removeItem: EventEmitter<ListItem> = new EventEmitter<ListItem>();
+  @Output() private addItem: EventEmitter<string> = new EventEmitter<string>();
+  @Output() private removeItem: EventEmitter<ListItem> = new EventEmitter<ListItem>();
 
   public onKeyDown(event: KeyboardEvent): void {
     if (event.keyCode === 13) {
@@ -35,10 +24,4 @@ export class RandomListComponent {
   public onClickRemoveItem(item: ListItem): void {
     this.removeItem.emit(item);
   }
-
-  public calculate(num: number): number {
-    console.log('Calculating...');
-    return fibonacci(num);
-  }
-
 }
